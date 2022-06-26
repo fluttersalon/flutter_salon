231ed21f5a0ddaa826318cb697178928b55b5e31
sakushin <sakushin.udemy@gmail.com>
2022-06-26 15:21:39 +0900

    tabを動的に変化させる方法を追加

--- lib/main.dart
+++ lib/main.dart
@@ -28,37 +28,53 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  List<String> _titles = [
+    "tab1",
+    "tab2",
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int selectedIndex = 0;
+
+  late TabController _tabController = _createNewTabController();
+
+  TabController _createNewTabController() => TabController(
+        vsync: this,
+        length: _titles.length,
+      )..addListener(() {
+          setState(() {
+            selectedIndex = _tabController.index;
+          });
+        });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: true,
+          tabs: _titles.map((title) => Tab(text: title)).toList(),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'selected tab: ${selectedIndex + 1}',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          _titles.add('tab ${_titles.length + 1}');
+          setState(() {
+            _tabController = _createNewTabController();
+          });
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
